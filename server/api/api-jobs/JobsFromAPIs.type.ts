c6f77a3bcf721ce3063bda1687f5162aa170fcd2
nirjankhadka <nirjan.khadka@gfinity.net>
2023-02-28 19:44:45 +0545

    feat: add slug column to all tables

---
+++ server/api/api-jobs/JobsFromAPIs.type.ts
@@ -0,0 +1,25 @@
+import { Company, ExperienceLevel, Job, Role, Location } from '@prisma/client'
+
+export interface RemotiveJob {
+  id: number;
+  url: string;
+  title: string;
+  company_name: string;
+  company_logo: string;
+  category: string;
+  tags: string[];
+  job_type: string;
+  publication_date: string;
+  candidate_required_location: string;
+  salary: string;
+  description: string;
+  company_logo_url: string;
+}
+
+export interface jobWithLinkedData extends Omit<Job, 'company_id' | 'location_id' | 'duration_id' | 'experienceLevel_id' | 'role_id'> {
+  company: Company;
+  location: Location;
+  duration: Duration;
+  experienceLevel: ExperienceLevel;
+  role: Role;
+}
--- server/api/api-jobs/remotive.ts
+++ server/api/api-jobs/remotive.ts
@@ -1,7 +1,24 @@
+import { Company } from '@prisma/client'
+import { jobWithLinkedData, RemotiveJob } from '~~/server/api/api-jobs/JobsFromAPIs.type'
+
 export default defineEventHandler(async () => {
-  const data = await (await fetch('https://remotive.io/api/remote-jobs?category=software-dev&limit=10'))
+  const data = await (await fetch('https://remotive.io/api/remote-jobs?category=software-dev&limit=1'))
 
-  const responseData = await data.json()
+  const responseData: {jobs: RemotiveJob[]} = await data.json()
 
-  return responseData.jobs
+  const jobsWithLinkedData = getJobsWithLinkedDataFromRemotiveJob(responseData.jobs)
+  return jobsWithLinkedData
 })
+
+const getJobsWithLinkedDataFromRemotiveJob = (jobs: RemotiveJob[]) => {
+  // const jobsWithLinkedData: jobWithLinkedData[] = jobs.map(job => {
+  //   // create all the linked data
+  //   const company: Company = {
+  //     name: job.company_name,
+
+  //   // return the job with all the linked data
+
+  // })
+
+  return jobs
+}
