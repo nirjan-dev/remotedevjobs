6fa58fc54e3b799183dcc533eca24e572ebea989
nirjankhadka <nirjan.khadka@gfinity.net>
2023-03-28 11:42:12 +0545

    fix: add debugging to remotok test endpoint with console log

--- server/api/api-jobs/remoteoktest.ts
+++ server/api/api-jobs/remoteoktest.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { logger } from '~/utils/logger'
 import { RemoteOkJob } from '~~/server/api/api-jobs/JobsFromAPIs.type'
 
@@ -5,17 +6,17 @@ export default defineEventHandler(async () => {
   let data
   try {
     data = await (await fetch('https://remoteok.com/api?api=1'))
-    logger.log('Fetched remoteok jobs', data.statusText)
+    console.log('Fetched remoteok jobs', data.statusText)
     data.headers.forEach((value, key) => {
-      logger.log(key, value)
+      console.log(key, value)
     })
     const responseData: RemoteOkJob[] = await data.json()
 
     return responseData
   } catch (error:any) {
-    logger.log(error.message, 'Error fetching remoteok jobs', data?.statusText)
+    console.log(error.message, 'Error fetching remoteok jobs', data?.statusText)
     data?.headers.forEach((value, key) => {
-      logger.log(key, value)
+      console.log(key, value)
     })
     return {
       error: error.message
