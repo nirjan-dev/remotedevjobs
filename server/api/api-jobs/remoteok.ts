44d5fc60609bddd53d3d87140c809ac5c6c1326b
nirjankhadka <nirjan.khadka@gfinity.net>
2023-03-29 10:08:50 +0545

    fix: finally fix the remoteok issue?

--- server/api/api-jobs/remoteok.ts
+++ server/api/api-jobs/remoteok.ts
@@ -7,12 +7,29 @@ import { logger } from '~/utils/logger'
 export default defineEventHandler(async (event: H3Event) => {
   let data
   try {
-    data = await (await fetch('https://remoteok.com/api?api=1'))
+    data = await (await fetch('https://remoteok.com/api?api=1', {
+      credentials: 'include',
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0',
+        Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        Pragma: 'no-cache',
+        'Cache-Control': 'no-cache'
+      },
+      method: 'GET',
+      mode: 'cors'
+    }))
   } catch (error:any) {
     logger.error(error.message, 'Error fetching remoteok jobs')
-    return {
-      error: error.message
-    }
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Error fetching remoteok jobs' + error.message
+    })
   }
 
   const responseData: RemoteOkJob[] = await data.json()
