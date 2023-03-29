8f5ef39f90ecdd01f3e6f3badf7455ef8d09454c
nirjankhadka <nirjan.khadka@gfinity.net>
2023-03-29 10:00:45 +0545

    fix: try the same exact fetch request from the browser

--- server/api/api-jobs/remoteoktest.ts
+++ server/api/api-jobs/remoteoktest.ts
@@ -1,23 +1,34 @@
 /* eslint-disable no-console */
-import { logger } from '~/utils/logger'
 import { RemoteOkJob } from '~~/server/api/api-jobs/JobsFromAPIs.type'
 
 export default defineEventHandler(async () => {
   let data
   try {
-    data = await (await fetch('https://remoteok.com/api?api=1'))
-    console.log('Fetched remoteok jobs', data.statusText)
-    data.headers.forEach((value, key) => {
-      console.log(key, value)
+    data = await fetch('https://remoteok.com/api?api=1', {
+      credentials: 'include',
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0',
+        Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        Pragma: 'no-cache',
+        'Cache-Control': 'no-cache'
+      },
+      method: 'GET',
+      mode: 'cors'
     })
-    const responseData: RemoteOkJob[] = await data.json()
+    console.log('Fetched remoteok jobs', data)
 
-    return responseData
+    return {
+      data
+    }
   } catch (error:any) {
     console.log(error.message, 'Error fetching remoteok jobs', data?.statusText, data, error)
-    data?.headers.forEach((value, key) => {
-      console.log(key, value)
-    })
+
     return {
       error: error.message
     }
