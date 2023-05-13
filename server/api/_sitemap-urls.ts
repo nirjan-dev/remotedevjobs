dcd3f1a0bc3ea61106b130b2b14a6d63312849c4
nirjankhadka <nirjan.khadka@gfinity.net>
2023-05-13 12:31:12 +0545

    fix: add blog posts to sitemap

--- server/api/_sitemap-urls.ts
+++ server/api/_sitemap-urls.ts
@@ -1,4 +1,6 @@
 import { H3Event } from 'h3'
+import { Post } from '~/types/blog.types'
+import { serverQueryContent } from '#content/server'
 
 export default cachedEventHandler(async (event:H3Event) => {
   const PrismaClient = event.context.prisma
@@ -10,10 +12,18 @@ export default cachedEventHandler(async (event:H3Event) => {
     }
   })
 
-  return jobs.map(job => ({
+  const jobRoutes = jobs.map(job => ({
     loc: `https://remotedevjobs.net/jobs/${job.slug}`,
     lastmod: job.updatedAt
   }))
+
+  const posts = await serverQueryContent<Post>(event).find()
+  const postRoutes = posts.map(post => ({
+    loc: `https://remotedevjobs.net/${post._path}`,
+    lastmod: new Date(post.dateModified ?? post.datePublished).toISOString()
+  }))
+
+  return [...jobRoutes, ...postRoutes]
 }, {
   name: 'sitemap-dynamic-urls',
   maxAge: 60 * 10 // cache URLs for 10 minutes
